04d6a1e8f12acf16c624fc6c7eed88afcb794910
Gabriel Lulu <glulu4444@gmail.com>
2025-07-28 18:57:09 -0400

    added number

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -295,7 +295,7 @@ export const Navigation: FunctionComponent = () => {
                       className="flex items-center space-x-3 py-2 px-4 rounded-lg hover:bg-white/10 transition-colors duration-200"
                     >
                       <Phone className="text-primaryCyan"  size="18" />
-                      <span>(123)-456-7890</span>
+                      <span>(203) 261-7800</span>
                     </a>
                     <a
                       href="mailto:sales@paragonexterior.com"
